84a1a5b852228fd58289913a53c2933c6ac9603c
CodeArc <CodeArctutorials@gmail.com>
2021-01-09 19:19:57 +0530

    card redirect link changed.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
-const cards = document.querySelectorAll('.card');
-const alternateRedirectLink = "http://www.codearc.ml/";
+const cards = document.querySelectorAll(".card");
+const alternateRedirectLink = "https://www.codearc.ml/";
 
 cards.forEach((card, index) => {
-    card.addEventListener('click', () => {
-        if (card.getAttribute('data-redirect')) {
-            window.location.href = card.getAttribute('data-redirect');
-        } else {
-            window.location.href = alternateRedirectLink;
-        }
-
-    });
+  card.addEventListener("click", () => {
+    if (card.getAttribute("data-redirect")) {
+      window.location.href = card.getAttribute("data-redirect");
+    } else {
+      window.location.href = alternateRedirectLink;
+    }
+  });
 });
